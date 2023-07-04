9cb74ef51a6f7bbfaab5757f3957d58f9e077063
gavinlai999 <gavinlai07@gmail.com>
2023-07-03 20:36:50 -0700

    a
    a

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+// instantiates Express
+const app = express();
+// for serving files in the public folder, such as CSS files
+app.use(express.static("./Window"));
+// server responds when the client sends a GET request for the / endpoint
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/Window/index.html");
+});
+app.post("/", function(req,res) {
+     res.sendFile(__dirname + "/Window/index2.html");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
+});
