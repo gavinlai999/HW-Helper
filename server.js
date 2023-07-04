ce31e2df42d2877618ca9d649cca0abd20bae2c8
gavinlai999 <gavinlai07@gmail.com>
2023-07-03 21:14:50 -0700

    we
    we

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 // instantiates Express
 const app = express();
 // for serving files in the public folder, such as CSS files
-app.use(express.static(".homescreen"));
+app.use(express.static("./Window/homescreen"));
 // server responds when the client sends a GET request for the / endpoint
 app.get("/", function(req,res) {
-    res.sendFile(__dirname + "homescreen/home.html");
+    res.sendFile(__dirname + "/Window/homescreen/home.html");
 });
 app.post("/", function(req,res) {
-     res.sendFile(__dirname + "createscreen/create.html");
+     res.sendFile(__dirname + "/Window/createscreen/create.html");
 });
 
 
