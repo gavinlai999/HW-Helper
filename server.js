05c227992704366086752632c0a27283806aeaf3
gavinlai999 <gavinlai07@gmail.com>
2023-07-03 22:45:47 -0700

    e
    e

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 // instantiates Express
 const app = express();
 // for serving files in the public folder, such as CSS files
-app.use(express.static("public"));
 app.use(express.static("./Window/homescreen"));
 // server responds when the client sends a GET request for the / endpoint
 app.get("/", function(req,res) {
