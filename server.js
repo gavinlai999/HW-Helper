91631301b79cee76bbf4821c3588545913e4adba
gavinlai999 <gavinlai07@gmail.com>
2023-07-05 14:59:42 -0700

    we
    we

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const rootDirectory = process.cwd();
 
-app.use(express.static(path.join(rootDirectory, "Windows")));
+app.use(express.static(path.join(__dirname, "Windows")));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(rootDirectory, "Windows", "homescreen", "home.html"));
+  res.sendFile(path.join(__dirname, "Windows", "homescreen", "home.html"));
 });
 
 app.post("/", function(req, res) {
-  res.sendFile(path.join(rootDirectory, "Windows", "createscreen", "create.html"));
+  res.sendFile(path.join(__dirname, "Windows", "createscreen", "create.html"));
 });
 
 app.post("/support", function(req, res) {
-  res.sendFile(path.join(rootDirectory, "Windows", "supportscreen", "support.html"));
+  res.sendFile(path.join(__dirname, "Windows", "supportscreen", "support.html"));
 });
 
 const port = process.env.PORT || 3000;
